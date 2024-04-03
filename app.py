e3502be09d2987770917aa724ea2dbc5718e2ccf
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-04-03 05:24:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,74 @@
-#from cs50 import SQL
-from flask import Flask, render_template, request
-# from flask_session import Session
-import openai, os
-# from dotenv import load_dotenv
+from flask import Flask, request, jsonify, render_template, send_file, Response
+from PIL import Image, ImageDraw, ImageFont
+import openai, requests, random
+from offices import char_dicts
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+char_dicts = char_dicts.Char_Dicts
+from bs4 import BeautifulSoup
+from kvsqlite.sync import Client
+from translate import Translator
+import json
+from io import BytesIO
+import arabic_reshaper, bidi.algorithm
+from elevenlabs import set_api_key, generate, Voice
 
-app = Flask(__name__)
+server = Flask(__name__)
 
-# this route takes you to the index (main) page to input the question
-# it will forward the prompt through the action form to answer.html 
+domains_data = [
+    {
+        "domain": "ChatGPT",
+        "visits": 0
+    },
+    {
+        "domain": "decor",
+        "visits": 0
+    },
+    {
+        "domain": "insta",
+        "visits": 0
+    },
+    {
+        "domain": "que",
+        "visits": 0
+    },
+    {
+        "domain": "loque",
+        "visits": 0
+    },
+    {
+        "domain": "gen image",
+        "visits": 0
+    },
+    {
+        "domain": "wanted image",
+        "visits": 0
+    },
+    {
+        "domain": "IG save",
+        "visits": 0
+    },
+    {
+        "domain": "to speech",
+        "visits": 0
+    },
+]
 
-@app.route("/")
-def mainroute():
-    return render_template("index.html")
 
-@app.route('/chatgpt', methods=['GET'])
+def Not_text():
+  return jsonify({'result': 'Plz Enter Text'})
+
+
+@server.route('/', methods=['GET'])
+def home():
+  return render_template("index-home.html")
+
+
+##################################################
+##################################################
+##################################################
+
+
+@server.route('/chatgpt', methods=['GET'])
 def open_ai():
   domains_data[0]["visits"] += 1
   text = request.args.get('query', '')
@@ -38,49 +91,167 @@ def open_ai():
         'error': str(e),
         'result': 'An internal server error occurred'
     }), 500
-)        
+
 
 ##################################################
 
 
-def create_image(profile_url):
-  # Load the background image
-  background_path = 'database/background2.png'
-  background = Image.open(background_path).convert('RGBA')
+@server.route('/decor', methods=['GET'])
+def decor_darco():
+  domains_data[1]["visits"] += 1
+  input_text = request.args.get('query', '')
+  if input_text == '':
+    return Not_text()
 
-  # Load the overlay image
-  profile_response = requests.get(profile_url)
-  profile_response.raise_for_status()
-  profile = Image.open(BytesIO(profile_response.content)).convert('RGBA')
+  results = []
+  for char_dict in char_dicts:
+    decorated_text = ''
+    for char in input_text:
+      found = False
+      for char_key in char_dict.keys():
+        if char.lower() == char_key:
+          decorated_text += char_dict[char_key]
+          found = True
+          break
+      if not found:
+        decorated_text += char
+    results.append(decorated_text)
 
-  # Resize overlay image
-  size = (441, 441)
-  profile = profile.resize(size, Image.LANCZOS)
-
-  # Paste overlay image onto the background
-  position = (148, 285)
-  background.paste(profile, position, profile)
-
-  # Save the result
-  img_bytes = BytesIO()
-  background.save(img_bytes, format="PNG")
-  img_bytes.seek(0)
-
-  return img_bytes
+  return jsonify({"result": results})
 
 
-@app.route('/wanted_image')
-def generate_image2():
-  domains_data[6]["visits"] += 1
-  profile_url = request.args.get(
-      'profile_url', 'https://telegra.ph/file/cd9ef2c65ea3e791cf419.jpg')
-  img_bytes = create_image(profile_url)
-  return send_file(img_bytes, mimetype='image/png')
-    
-    ##################################################
+##################################################
 
 
-    @app.route('/gen_image')
+@server.route('/insta', methods=['GET'])
+def get_instagram_data():
+  domains_data[2]["visits"] += 1
+  target = request.args.get('query', '')
+
+  if not target:
+    return jsonify({"error": "Username must be provided"}), 400
+  username = target
+
+  url = f'https://www.instagram.com/api/v1/users/web_profile_info/?username={username}'
+
+  headers = {
+      'Accept': '*/*',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'ar,en-US;q=0.9,en;q=0.8',
+      'Cookie':
+      'ig_did=FE857FA2-50E2-4412-9205-AFD556AC313B; ig_nrcb=1; mid=ZDAWPwALAAFNxlM9KwFP3jLaQ3kC; datr=rRYwZAFC-LaBsBCekyGPVduH; ds_user_id=47675279829; csrftoken=0VNXCy4bFu3vwedso6fsONvbPeGlY3Vp; sessionid=47675279829%3ACyLAsPHeA8XBs7%3A25%3AAYeDeNb7cAY0wyB0CQk0bLC13K2Gav7gXzCTiL14pQ; shbid="14281\05447675279829\0541731245278:01f7b8cde814936c5d2535e3d717d05f128df2e36b37709553bdadfb33922661aab5d39f"; shbts="1699709278\05447675279829\0541731245278:01f769156ff38adbc65dcb561941e80a130c4844e635ab9400ec7642602f17ee8c84f2a7"; rur="CLN\05447675279829\0541731245518:01f748af0c558f3af9fdb806a036e1abf37800c7846bacf74bf63badc2a95bd23e30316e"',
+      'Dpr': '1',
+      'Referer': f'https://www.instagram.com/{username}/',
+      'Sec-Ch-Prefers-Color-Scheme': 'dark',
+      'Sec-Ch-Ua':
+      '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+      'Sec-Ch-Ua-Full-Version-List':
+      '"Google Chrome";v="119.0.6045.124", "Chromium";v="119.0.6045.124", "Not?A_Brand";v="24.0.0.0"',
+      'Sec-Ch-Ua-Mobile': '?0',
+      'Sec-Ch-Ua-Model': '""',
+      'Sec-Ch-Ua-Platform': '"Windows"',
+      'Sec-Ch-Ua-Platform-Version': '"10.0.0"',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-origin',
+      'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+      'Viewport-Width': '418',
+      'X-Asbd-Id': '129477',
+      'X-Csrftoken': '0VNXCy4bFu3vwedso6fsONvbPeGlY3Vp',
+      'X-Ig-App-Id': '936619743392459',
+      'X-Ig-Www-Claim':
+      'hmac.AR1mGqbAXDS4qLXVl56NWM6cwzfZ8E_jnLgMQ4U-DqbkDirp',
+      'X-Requested-With': 'XMLHttpRequest'
+  }
+
+  data_json = {}
+  dk = requests.get(url, headers=headers)
+  data = dk.json()
+
+  data_json['id'] = (data['data']['user']['id'])
+  data_json['profile pic'] = (data['data']['user']['profile_pic_url_hd'])
+  data_json['full name'] = (data['data']['user']['full_name'])
+  data_json['bio'] = (data['data']['user']['biography'])
+
+  if "bio_links" in data['data']['user'] and len(
+      data['data']['user']['bio_links']) > 0:
+    first_bio_link = data['data']['user']['bio_links'][0]
+    if "url" in first_bio_link:
+      data_json['bio link'] = first_bio_link['url']
+    else:
+      data_json['bio link'] = None
+  else:
+    data_json['bio link'] = None
+
+  data_json['posts'] = (
+      data['data']['user']['edge_owner_to_timeline_media']['count'])
+  data_json['follow'] = (data['data']['user']['edge_followed_by']['count'])
+  data_json['following'] = (data['data']['user']['edge_follow']['count'])
+  data_json['private'] = (data['data']['user']['is_private'])
+  data_json['business account'] = (data['data']['user']['is_business_account'])
+  data_json['professional account'] = (
+      data['data']['user']['is_professional_account'])
+  data_json['status'] = (data['status'])
+
+  return jsonify({"result": data_json})
+
+
+##################################################
+
+
+@server.route('/que', methods=['GET'])
+def Question():
+  domains_data[3]["visits"] += 1
+  text = request.args.get('query', '')
+  if text != 'dark man':
+    return jsonify({"result": 'An internal server error occurred'}), 500
+
+  db = Client("database/ktweet.daddy")
+  gg = []
+  for i in db.keys():
+    gg.append(i[0])
+  gs = random.randint(1, 355)
+  answer = (gg[gs])
+
+  return jsonify({"result": answer})
+
+
+##################################################
+
+
+@server.route('/loque', methods=['GET'])
+def loQuestion():
+  domains_data[4]["visits"] += 1
+  text = request.args.get('query', '')
+  if text != 'dark man':
+    return jsonify({"result": 'An internal server error occurred'}), 500
+
+  db = Client("database/ah.daddy")
+  translator = Translator(to_lang="ar")
+
+  while True:
+    # توليد مفتاح عشوائي
+    random_key = str(random.randint(1, 489))  # ضبط النطاق حسب احتياجاتك
+    # الحصول على القيمة للمفتاح العشوائي
+    value = db.get(random_key)
+
+    if value is not None:
+      data = {}
+      data['one'] = {
+          'question': translator.translate(value['one']['question'])
+      }
+      data['two'] = {
+          'question': translator.translate(value['two']['question'])
+      }
+      return jsonify({"result": data})
+      break  # انهاء الحلقة عند العثور على قيمة صالحة
+
+
+##################################################
+
+
+@server.route('/gen_image')
 def generate_image():
   domains_data[5]["visits"] += 1
   profile_url = request.args.get(
@@ -187,3 +358,174 @@ def create_profile_image(profile_url, second_profile_url, text_lines):
   img_bytes.seek(0)
 
   return img_bytes
+
+
+##################################################
+
+
+def create_image(profile_url):
+  # Load the background image
+  background_path = 'database/background2.png'
+  background = Image.open(background_path).convert('RGBA')
+
+  # Load the overlay image
+  profile_response = requests.get(profile_url)
+  profile_response.raise_for_status()
+  profile = Image.open(BytesIO(profile_response.content)).convert('RGBA')
+
+  # Resize overlay image
+  size = (441, 441)
+  profile = profile.resize(size, Image.LANCZOS)
+
+  # Paste overlay image onto the background
+  position = (148, 285)
+  background.paste(profile, position, profile)
+
+  # Save the result
+  img_bytes = BytesIO()
+  background.save(img_bytes, format="PNG")
+  img_bytes.seek(0)
+
+  return img_bytes
+
+
+@server.route('/wanted_image')
+def generate_image2():
+  domains_data[6]["visits"] += 1
+  profile_url = request.args.get(
+      'profile_url', 'https://telegra.ph/file/cd9ef2c65ea3e791cf419.jpg')
+  img_bytes = create_image(profile_url)
+  return send_file(img_bytes, mimetype='image/png')
+
+
+##################################################
+@server.route('/instagramdl')
+def instagramdl():
+  domains_data[7]["visits"] += 1
+  reel = request.args.get('qurey', '')
+  url = 'https://vidinsta.app/web/home/fetch'
+  headers = {
+      "Accept": "text/html, */*; q=0.01",
+      "Accept-Encoding": "identity;q=1, *;q=0",  #"gzip, deflate, br",
+      "Accept-Language": "ar,en-US;q=0.9,en;q=0.8",
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "Cookie":
+      "_csrf=0c6b1b278b7095293e8bc4695dd1091f8fbfa2514d8abb4369ee0579d5ba2b3da%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22CBrXo9IGCrIKivmlwzarr7eMQl703rNa%22%3B%7D; _ga_V3DS4P6657=GS1.1.1701613588.1.0.1701613588.0.0.0; _ga=GA1.1.302626962.1701613588; __gads=ID=92d242f839780292:T=1701613541:RT=1701613541:S=ALNI_MZ43H0MHddpxP3zL5IdT0zRsgPbPA; __gpi=UID=00000ce28d87d8f2:T=1701613541:RT=1701613541:S=ALNI_MacxKflLJS08Cwcp0UAE22yAW0XOA; _ga_C685S7JGC5=GS1.1.1701613588.1.0.1701613593.0.0.0",
+      "Origin": "https://vidinsta.app",
+      "Referer": "https://vidinsta.app/",
+      "Sec-Ch-Ua":
+      "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+      "Sec-Ch-Ua-Mobile": "?0",
+      "Sec-Ch-Ua-Platform": "\"Windows\"",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-origin",
+      "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+      "X-Csrf-Token":
+      "-gCkoW2dMnp4KsM6Hz9oMJgPCV-wRxIQvBN7kXbbBzq5Qtb5AqR7PTtYinF2SQVc73VoLcJwd13tf0yhRalJWw==",
+      "X-Requested-With": "XMLHttpRequest",
+  }
+
+  data = {
+      'url': reel,
+      "type": "",
+  }
+
+  response = requests.post(url, headers=headers, data=data)
+  print(response.status_code)
+  print(response.text)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  footer_div = soup.find('div', class_='col-xs-4 text-center')
+  footer_div2 = soup.find('div', class_='col-sm-4 col-sm-pull-8')
+
+  data = {}
+  #video
+  if footer_div:
+    anchor_tag = footer_div.find('a')
+    href_value = anchor_tag.get('href')
+    vid = "https://vidinsta.app" + href_value
+    data['video'] = (vid)
+
+#image
+  if footer_div2:
+    anchor_tag = footer_div2.find('img')
+    href_value = anchor_tag.get('src')
+    img = "https://vidinsta.app" + href_value
+    data['image'] = (img)
+    print(data)
+
+  return jsonify({"result": data})
+
+
+##################################################
+
+
+@server.route('/to-speech', methods=['GET'])
+def text_to_speech():
+  domains_data[8]["visits"] += 1
+
+  dktext = request.args.get('query', '')
+  apikey = request.args.get('apikey', '')
+  if apikey == "":
+    return jsonify({"error": "apikey is empty"})
+  if dktext == "":
+    return jsonify({"error": "text is empty"})
+
+  set_api_key(f"{apikey}")
+
+  audio = generate(text=dktext, voice="Adam", model="eleven_multilingual_v2")
+
+  if audio:
+    # احفظ البيانات الصوتية في ملف مؤقت
+    audio_path = 'temp_audio.mp3'
+    with open(audio_path, 'wb') as f:
+      f.write(audio)
+
+      return send_file(audio_path,
+                       mimetype='audio/mp3',
+                       as_attachment=False,
+                       download_name='output.mp3')
+
+
+##################################################
+
+
+@server.route('/to-speech-v2', methods=['GET'])
+def text_to_speech2():
+  domains_data[8]["visits"] += 1
+
+  dktext = request.args.get('query', '')
+  apikey = request.args.get('apikey', '')
+  if apikey == "":
+    return jsonify({"error": "apikey is empty"})
+  if dktext == "":
+    return jsonify({"error": "text is empty"})
+
+  set_api_key(f"{apikey}")
+
+  audio = generate(text=dktext, voice="Mimi", model="eleven_multilingual_v2")
+
+  if audio:
+    audio_path = 'temp_audio2.mp3'
+    with open(audio_path, 'wb') as f:
+      f.write(audio)
+
+      #tts
+      return send_file(audio_path,
+                       mimetype='audio/mp3',
+                       as_attachment=False,
+                       download_name='output2.mp3')
+
+
+##################################################
+
+
+@server.route("/stats", methods=["GET"])
+def show_stats():
+  return render_template("index-stats.html", domains_data=domains_data)
+
+
+##################################################
+if __name__ == '__main__':
+  server.run(host='0.0.0.0', port=1337)
